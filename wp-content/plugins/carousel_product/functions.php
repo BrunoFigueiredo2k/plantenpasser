bd691cb951814bd8787a09569c7ce61177f45cfd
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2021-03-31 00:58:03 +0200

    plugins setup en library

--- wp-content/plugins/carousel_product/functions.php
+++ wp-content/plugins/carousel_product/functions.php
@@ -7,112 +7,52 @@
     Author URI: plantenpasser.nl
     Version: 1.0
 */
+
+define('PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('CSS_COMPONENTS_DIR', PLUGIN_DIR . 'css_components/');
+define('JS_COMPONENTS_DIR', PLUGIN_DIR . 'js_components/');
+
 add_shortcode('carousel_shortcode', 'product_carousel');
 function product_carousel(){
+  include_component(CSS_COMPONENTS_DIR . 'carousel.css');
   ?>
-    <style>
-    .mySlides {display: none}
-    img {vertical-align: middle;}
-
-    .mySlides img{
-        margin: 0 auto;
-    }
-    
-    /* Slideshow container */
-    .slideshow-container {
-      max-width: 1000px;
-      position: relative;
-      margin: auto;
-    }
-    
-    /* Next & previous buttons */
-    .prev, .next {
-      cursor: pointer;
-      position: absolute;
-      top: 50%;
-      width: auto;
-      padding: 16px;
-      margin-top: -22px;
-      color: white;
-      font-weight: bold;
-      font-size: 18px;
-      transition: 0.6s ease;
-      border-radius: 0 3px 3px 0;
-      user-select: none;
-      background: black;
-    }
-    
-    /* Position the "next button" to the right */
-    .next {
-      right: 0;
-      border-radius: 3px 0 0 3px;
-    }
-    
-    /* On hover, add a black background color with a little bit see-through */
-    .prev:hover, .next:hover {
-      background-color: rgba(0,0,0,0.8);
-    }
-    
-    /* Caption text */
-    .text {
-      color: #f2f2f2;
-      font-size: 15px;
-      padding: 8px 12px;
-      position: absolute;
-      bottom: 8px;
-      width: 100%;
-      text-align: center;
-    }
-    
-    /* Number text (1/3 etc) */
-    .numbertext {
-      color: #f2f2f2;
-      font-size: 12px;
-      padding: 8px 12px;
-      position: absolute;
-      top: 0;
-    }
-    
-    /* Fading animation */
-    .fade {
-      -webkit-animation-name: fade;
-      -webkit-animation-duration: 1.5s;
-      animation-name: fade;
-      opacity: 1;
-      animation-duration: 1.5s;
-    }
-    
-    @-webkit-keyframes fade {
-      from {opacity: .4} 
-      to {opacity: 1}
-    }
-    
-    @keyframes fade {
-      from {opacity: .4} 
-      to {opacity: 1}
-    }
-    
-    /* On smaller screens, decrease text size */
-    @media only screen and (max-width: 300px) {
-      .prev, .next,.text {font-size: 11px}
-    }
-    </style>
+    <?php 
+      // Data model
+      $data = array(
+        'plants' => array(
+          'name' => '',
+          'price' => '',
+          'length' => '',
+          'width' => '',
+          'img_url' => '',
+        ),
+        'pots' => array(
+          'name' => '',
+          'price' => '',
+          'length' => '',
+          'width' => '',
+          'color' => '',
+          'img_url' => '',
+        )
+      );
+    ?>
 
     <!-- Slideshow container -->
     <div class="slideshow-container">
     
       <!-- Full-width images with number and caption text -->
+      <?php
+      foreach ($data['plants'] as $key => $plant) { ?>
+        <div class="mySlides fade">
+          <img src="<?php $plant['img_url'] ?>">
+        </div>
+      <?php } ?>
+      
+<!--     
       <div class="mySlides fade">
-        <div class="numbertext">1 / 3</div>
-        <img src="http://localhost/plantenpassernl/wp-content/uploads/2021/03/pot.jpg">
-        <div class="text">Caption Text</div>
-      </div>
-    
-      <div class="mySlides fade">
-        <div class="numbertext">2 / 3</div>
         <img src="http://localhost/plantenpassernl/wp-content/uploads/2021/03/plant-300x300.jpg">
         <div class="text">Caption Two</div>
-      </div>
+      </div> -->
     
       <!-- Next and previous buttons -->
       <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
@@ -120,30 +60,7 @@ function product_carousel(){
     </div>
     <br>
 
-    <script>
-    var slideIndex = 1;
-    showSlides(slideIndex);
-    
-    // Next/previous controls
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
-    
-    // Thumbnail image controls
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
-    
-    function showSlides(n) {
-      var i;
-      var slides = document.getElementsByClassName("mySlides");
-      if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-      }
-      slides[slideIndex-1].style.display = "block";
-    }
-    </script>
+    <?php include_component(JS_COMPONENTS_DIR . 'carousel.js'); ?>
+      
     <?php
 }
---
+++ wp-content/plugins/plantenpasser_library/functions.php
@@ -0,0 +1,34 @@
+<?php
+/*
+    Plugin Name: Plantenpasser - Library
+    Plugin URI: https://plantenpasser.nl/
+    Description: Library of plantenpasser developer team (all reusable functions, classes, etc.)
+    Author: plantenpasser
+    Author URI: plantenpasser.nl
+    Version: 1.0
+*/
+/** ----------- GLOBALS ----------- */
+
+
+/** ----------- FUNCTIONS ----------- */
+/** Includes components from passed directory (html, css, js) */
+function include_component($abs_file_dir) {
+    $content = file_get_contents($abs_file_dir);
+    $exploded_dir_path = explode(".", $abs_file_dir);
+    $extension = strtolower($exploded_dir_path[count($exploded_dir_path ) - 1]);
+  
+    // Check content type
+    switch ($extension) {
+      case 'html':
+        echo $content;
+        break;
+      case 'js':
+        echo "<script>".$content."</script>";
+        break;
+      case 'css':
+        echo "<style>".$content."</style>";
+        break;
+    }
+}
+
+/** ----------- CLASSES ----------- */
